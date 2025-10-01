455d6aca1a4e76957a04fa3c655b346910e74045
taeseok22 <dhxotjr12345@naver.com>
2025-10-01 17:45:33 +0900

    20251001

--- js/10000hours.js
+++ js/10000hours.js
@@ -1,1 +1,9 @@
-// js file
+const startbutton = document.getElementById('start-button');
+
+startbutton.addEventListener('click', ()=>{
+  const firstinputadd = document.getElementById('firstinput').value;
+  const secondinputadd = document.getElementById('secondinput').value;
+  document.getElementById('firstmessage').innerText = firstinputadd;
+  document.getElementById('secondmessage').innerText = 10000 / secondinputadd ;
+})
+
